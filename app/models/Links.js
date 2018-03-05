5141bc8882d76bf25029b681e65edc01f3c28541
Zames <zames.chua@gmail.com>
2018-03-06 02:28:54 +0800

    Minor naming changes

---
+++ app/models/Links.js
@@ -0,0 +1,45 @@
+// Setup Database
+const mysql = require('mysql2/promise');
+
+console.log("Connecting to mySQL database...");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "herochua",
+  password: "9517503a",
+  database: "custom-link"
+}).then(connection => {
+  console.log("Connected to mySQL database!");
+
+  const Links = {
+    create: async (sourceLink, targetUrl) => {
+      // Enter info into database
+      const command = `
+        INSERT INTO links (sourceLink, targetUrl)
+        VALUES (?, ?)
+        `;
+      const result = await connection.execute(command, [sourceLink, targetUrl]);
+      return result[0];
+    },
+    read: async (sourceLink) => {
+      const command = `
+        SELECT targetUrl
+        FROM links
+        WHERE sourceLink = ?
+        `;
+      const result = await connection.execute(command, sourceLink);
+      return result[0].targetUrl;
+    },
+    update: async (sourceLink, targetUrl) => {
+      // Enter info into database
+      const command = `
+        UPDATE links
+        SET targetUrl = ?
+        WHERE sourceLink = ?
+        `;
+      const result = await connection.execute(command, [targetUrl, sourceLink]);
+      return result[0];
+    }
+  }
+
+  module.exports = Links;
+});
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -17,34 +17,47 @@ router.route('/')
 // Creating a new link
 router.route('/api')
   .post((req, res) => {
-    const from = req.query.from;
-    const to = req.query.to;
-
+    const sourceLink = req.query.sourceLink;
+    const targetUrl = req.query.targetUrl;
+    const command = `
+      INSERT INTO links (sourceLink, targetUrl)
+      VALUES (sourceLink, targetUrl)
+      `
     // Enter info into database
-    connection.query(`INSERT INTO links (sourceLink, targetUrl) VALUES ('${from}', '${to}')`, function (error, results, fields) {
+    connection.query(command, function (error, results, fields) {
       if (error) throw error;
-      res.end(`Link successfully created! customl.ink/${from} now links to ${to}`);
+      res.end(`Link successfully created! customl.ink/${sourceLink} now links to ${targetUrl}`);
     });
   })
+
+// Accessing a custom link
+router.route('/:sourceLink')
+  .get((req, res) => {
+    // Enter info into database
+    const command = `
+      SELECT targetUrl
+      FROM links
+      WHERE sourceLink = '${req.params.from}'
+      `
+    connection.query(command, function (error, results, fields) {
+      if (error) throw error;
+      res.redirect(results[0].targetUrl);
+    });
+  })
+  /*
   .put((req, res) => {
     const sourceLink = req.query.from;
     const targetUrl = req.query.to;
-
+    const command = `
+      UPDATE links
+      SET targetUrl = '${targetUrl}'
+      WHERE sourceLink = '${sourceLink}'
+    `
     // Enter info into database
-    connection.query(`UPDATE links SET targetUrl = '${targetUrl}' WHERE sourceLink = '${sourceLink}'`, function (error, results, fields) {
+    connection.query(command, function (error, results, fields) {
       if (error) throw error;
       res.end(`Link successfully updated! customl.ink/${sourceLink} now links to ${targetUrl}`);
     });
   });
-
-// Accessing a custom link
-router.route('/:from')
-  .get((req, res) => {
-    // Enter info into database
-    connection.query(`SELECT targetUrl FROM links WHERE sourceLink = '${req.params.from}'`, function (error, results, fields) {
-      if (error) throw error;
-      res.redirect(results[0].targetUrl);
-    });
-  });
-
+*/
 module.exports = router;
